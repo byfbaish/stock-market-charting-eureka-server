6cebb33c4888ae43e56b8ce41181bfc945b5db94
Yun Fei Bai <byfbaish@cn.ibm.com>
2019-12-04 16:45:50 +0900

    first commit

---
+++ src/test/java/com/FTD/stockmarketchartingeurekaserver/StockMarketChartingEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FTD.stockmarketchartingeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketChartingEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
